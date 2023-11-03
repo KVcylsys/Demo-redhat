001ec9270cfe05d5794e0b7227aaeeaf71a743fd
KVcylsys <134579016+KVcylsys@users.noreply.github.com>
2023-11-03 18:23:12 +0530

    dfd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './components/hello'; 
 
 function App() {
   return (
-    <Hello /> 
+    <div className="App mt-2 mr-5 d-flex flex-column">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <button className="btn btn-primary">Click me</button>
+    </div>
   );
 }
 
